778c06afbc5d2ba5747499ff13a0e3d837ca233b
cjdRevature <collin027@revature.net>
2023-04-01 16:10:22 -0400

    added mega millions generator

---
+++ random-number-generators/mega-millions.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  1 15:43:06 2023
+
+@author: collin
+
+program to generate random numbers for mega millions
+
+"""
+
+import random
+
+# define range
+min = 1
+max = 70
+
+# set of numbers, list of numbers
+nums_set = set()
+nums_list = []
+
+# generate 5 numbers
+while len(nums_set) < 5:
+    num = random.randint(min, max)
+    nums_set.add(num)
+
+# set to list
+for num in nums_set:
+    nums_list.append(num)
+    
+# sort numbers
+nums_list.sort()
+
+# print numbers to console  
+print()  
+print("***Your Mega Millions numbers***")
+for num in nums_list:
+    print(str(num) + " ", end="")
+    
+print()
+print()
+print("***MEGABALL***")
+print(random.randint(1, 25))
